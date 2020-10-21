e910f12c7dd8afdc86586f8cc00a0433c9bc4a59
AbiamRemacheGonzalez <abiamrg.14@gmail.com>
2020-10-21 12:37:26 +0100

    Kata2Version3

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -7,10 +7,8 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Integer [] data ={2,2,3,4,5,7,7,8,9,2,2,3,5,7};
-        Map<Integer,Integer> histogram = new HashMap<>();
-        for (int i = 0; i < data.length; i++) {
-            histogram.put(data[i],histogram.containsKey(data[i]) ? histogram.get(data[i])+1: 1);
-        }
+        Histogram meta_histogram = new Histogram(data);
+        Map<Integer,Integer> histogram = meta_histogram.getHistogram();
         
         Iterator<Map.Entry<Integer,Integer>> entries = histogram.entrySet().iterator();
         while(entries.hasNext()){
